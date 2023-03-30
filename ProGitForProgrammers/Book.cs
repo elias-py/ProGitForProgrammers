4de520f9d8814a5390c95cdb3b1294489e115278
elias-py <eliascaceresvaldez837@gmail.com>
2023-03-30 11:37:50 -0400

    Book.cs added

---
+++ ProGitForProgrammers/Book.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProGitForProgrammers {
+    public class Book {
+        public string Title {get; set;}
+        public List<string> Authors {get; set;}
+
+        public DateTime PublicationDate {get; set;}
+    }
+}
